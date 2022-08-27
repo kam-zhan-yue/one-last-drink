e91bfade68b458433fc1e377be495d40b9f97f2d
Alexander Kam <kamzhanyue@gmail.com>
2022-08-27 16:47:30 +1000

    Implement Fairy Drink

--- Assets/Scripts/System/Drink.cs
+++ Assets/Scripts/System/Drink.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Drink : ScriptableObject
 {
     public Sprite sprite;
+    public Sprite openedSprite;
     public Color colour;
     [TextArea]
     public string description = string.Empty;
--- Assets/Scripts/UI/DrinkPopupItem.cs
+++ Assets/Scripts/UI/DrinkPopupItem.cs
@@ -6,26 +6,33 @@ using UnityEngine.UI;
 public class DrinkPopupItem : MonoBehaviour
 {
     public BarPopup barPopup;
-    public SpriteRenderer spriteRenderer;
     public Image image;
     private Drink drink;
 
     public void Init(Drink _drink)
     {
         drink = _drink;
+        ResetImage();
+    }
+
+    private void ResetImage()
+    {
         if (drink.sprite != null)
-            spriteRenderer.sprite = drink.sprite;
-        else if(image != null)
+            image.sprite = drink.sprite;
+        else if (image != null)
             image.color = drink.colour;
     }
 
     public void OnPointerEnter()
     {
+        if (drink.openedSprite != null)
+            image.sprite = drink.openedSprite;
         barPopup.PointerEnterDrink(drink);
     }
 
     public void OnPointerExit()
     {
+        ResetImage();
         barPopup.PointerExitDrink(drink);
     }
 
